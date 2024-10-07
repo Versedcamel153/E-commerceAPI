93ad4fe87fe32b8d7139040ba289596ad4f1462a
Versedcamel153 <93608676+Versedcamel153@users.noreply.github.com>
2024-10-07 20:44:41 +0000

    Update urls.py

--- ecommerceAPI/urls.py
+++ ecommerceAPI/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
@@ -26,5 +28,6 @@ urlpatterns = [
 
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
